04170701ddf19aed8b6ad65301510120190bb8ac
George Yarr <2553638y@student.gla.ac.uk>
2023-10-03 16:20:28 +0100

    port 3331, the old trusty

--- game/frontend/vite.config.ts
+++ game/frontend/vite.config.ts
@@ -3,6 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        port: 3331,
+    },
     base: "/graph-algorithm-game/",
     plugins: [vue()],
 });
