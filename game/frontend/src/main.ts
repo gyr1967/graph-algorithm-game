6917b364c5ee70341f5e7d6e2fd763e65d697d29
George Yarr <2553638y@student.gla.ac.uk>
2023-10-31 11:23:51 +0000

    separate pages for BFS's

--- game/frontend/src/main.ts
+++ game/frontend/src/main.ts
@@ -3,7 +3,10 @@ import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
 import Home from "./pages/Home.vue";
 import Game from "./pages/Game.vue";
-import BFS from "./pages/BFS.vue";
+import BFS from "./pages/BFS/BFS.vue";
+import BFSVis from "./pages/BFS/BFSVis.vue";
+import BFSGuided from "./pages/BFS/BFSGuided.vue";
+import BFSSolo from "./pages/BFS/BFSSolo.vue";
 import "./style.css";
 
 const root = "/graph-algorithm-game";
@@ -19,6 +22,18 @@ const routes = [
     {
         path: `${root}/game/bfs`,
         component: BFS,
+    },
+    {
+        path: `${root}/game/bfs/visualise`,
+        component: BFSVis,
+    },
+    {
+        path: `${root}/game/bfs/guided`,
+        component: BFSGuided,
+    },
+    {
+        path: `${root}/game/bfs/solo`,
+        component: BFSSolo,
     },
 ];
 
---
+++ game/frontend/src/pages/BFS/BFSSolo.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Graph from "../../components/Graph.vue";
+import GraphSizeSelector from "../../components/GraphSizeSelector.vue";
+import BFSPseudo from "../../components/BFSPseudo.vue";
+import { ref } from "vue";
+const graphSize = ref(1);
+</script>
+
+<template>
+    Solo
+    <div class="grid grid-cols-3">
+        <div class="">
+            <BFSPseudo />
+        </div>
+        <div class="text-center">
+            <Graph :scaling-factor="graphSize" />
+            <GraphSizeSelector v-model="graphSize" />
+        </div>
+    </div>
+</template>
