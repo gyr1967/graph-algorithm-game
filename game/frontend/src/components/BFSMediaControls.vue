affbac7f97e29a65a9fdd209ca67065782b4f67c
George Yarr <2553638y@student.gla.ac.uk>
2023-11-08 15:05:06 +0000

    minor style changes in media controls

--- game/frontend/src/components/BFSMediaControls.vue
+++ game/frontend/src/components/BFSMediaControls.vue
@@ -5,21 +5,21 @@ defineEmits(["startBFS", "nextStepBFS", "prevStepBFS"]);
 <template>
     <button
         :disabled="started"
-        class="bg-white text-black rounded-sm p-1 mr-1 disabled:bg-gray-600"
+        class="bg-white text-black rounded-sm p-1 mr-1 disabled:bg-gray-600 disabled:cursor-not-allowed"
         @click="$emit('startBFS')"
     >
         Start
     </button>
     <button
         :disabled="!started"
-        class="bg-white text-black rounded-sm p-1 mr-1 disabled:bg-gray-600"
+        class="bg-white text-black rounded-sm p-1 ml-1 mr-1 disabled:bg-gray-600 disabled:cursor-not-allowed"
         @click="$emit('prevStepBFS')"
     >
-        Previous
+        Previous (TODO)
     </button>
     <button
         :disabled="!started"
-        class="bg-white text-black rounded-sm p-1 ml-1 disabled:bg-gray-600"
+        class="bg-white text-black rounded-sm p-1 ml-1 disabled:bg-gray-600 disabled:cursor-not-allowed"
         @click="$emit('nextStepBFS')"
     >
         Next
