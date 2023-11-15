80be1681fbc6854333e77014e35edc945a44a08a
George Yarr <2553638y@student.gla.ac.uk>
2023-11-15 14:55:38 +0000

    styling

--- game/frontend/src/components/BFSSidePanel.vue
+++ game/frontend/src/components/BFSSidePanel.vue
@@ -5,14 +5,15 @@ defineProps<{
 }>();
 </script>
 <template>
-    <div class="border border-white p-4 rounded-md shadow-md">
-        <div class="mb-2 flex items-center">
+    <div class="border border-white p-3 rounded-md shadow-md grid grid-cols-2">
+        <div class="mb-2">
+            <div class="font-bold">Variables</div>
             <span class="font-bold">V:</span>
             <span class="ml-2">{{ currentVertexName }}</span>
         </div>
-        <div class="flex items-center">
-            <span class="font-bold mr-2">Queue:</span>
-            <div class="flex">
+        <div>
+            <span class="font-bold mr-2">Queue</span>
+            <div>
                 <div
                     v-for="(item, index) in currentQueue"
                     :key="index"
