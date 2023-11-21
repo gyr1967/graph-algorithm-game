04ae3d572f6807f0dd05e64185adfb246131f40b
George Yarr <2553638y@student.gla.ac.uk>
2023-11-21 15:47:40 +0000

    hintbox (unfinished)

---
+++ game/frontend/src/components/HintBox.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{
+    text: string;
+}>();
+</script>
+<template>
+    <div class="border border-white p-4 rounded-md shadow-md">
+        <span>{{ text }}</span>
+    </div>
+</template>
