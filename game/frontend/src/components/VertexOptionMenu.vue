d8812a77f27530df93a66ef5a56b76b0d095f511
George Yarr <2553638y@student.gla.ac.uk>
2023-11-21 15:48:41 +0000

    vertex option buttons now emit event

--- game/frontend/src/components/VertexOptionMenu.vue
+++ game/frontend/src/components/VertexOptionMenu.vue
@@ -1,17 +1,32 @@
 <script setup lang="ts">
 defineProps<{
+    text: string;
     nodeId: string;
 }>();
+defineEmits(["add-to-queue", "visit", "set-to-current"]);
 </script>
 <template>
+    <div class="flex justify-center">
+        <span>{{ text }}</span>
+    </div>
     <div class="flex justify-center items-center">
-        <span>{{ nodeId }}</span>
-        <button class="border border-white p-1 rounded-sm ml-1">
+        <button
+            class="border border-white p-1 rounded-sm ml-1"
+            @click="$emit('add-to-queue', nodeId)"
+        >
             Add to Queue
         </button>
-        <button class="border border-white p-1 rounded-sm ml-1">Visit</button>
-        <button class="border border-white p-1 rounded-sm ml-1">
-            Set to v
+        <button
+            class="border border-white p-1 rounded-sm ml-1"
+            @click="$emit('visit', nodeId)"
+        >
+            Visit
+        </button>
+        <button
+            class="border border-white p-1 rounded-sm ml-1"
+            @click="$emit('set-to-current', nodeId)"
+        >
+            Set to current vertex
         </button>
     </div>
 </template>
