7e7b5bc540472818cb201a03e3643fc5e3180296
George Yarr <2553638y@student.gla.ac.uk>
2023-11-28 15:32:52 +0000

    blur the hint for diy

--- game/frontend/src/components/HintBox.vue
+++ game/frontend/src/components/HintBox.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import Vertex from "../graph/Vertex";
 import { GuidedSteps } from "../types/BFS";
 defineProps<{
@@ -8,28 +9,49 @@ defineProps<{
     visited: Vertex[];
     queue: string[];
 }>();
+const obscureHint = ref<boolean>(true);
 </script>
 <template>
     <div class="border border-white p-4 rounded-md shadow-md">
-        <div v-if="!started">
-            <span>Click start!</span>
-        </div>
-        <div v-else>
-            <span v-if="text === 'add-to-queue' && visited.length > 0">
-                Add all of {{ currentVertexName }}'s neighbours (that aren't
-                already in the queue or visited) to the queue</span
-            >
-            <span v-else-if="text === 'add-to-queue' && visited.length === 0">
-                Add {{ currentVertexName }} to the queue</span
-            >
-            <span v-else-if="text === 'remove-and-set-to-current'"
-                >Remove {{ queue[0] }} from the queue and set it as the current
-                vertex</span
-            >
-            <span v-else-if="text === 'visit'"
-                >Mark {{ currentVertexName }} as visited</span
-            >
-            <span v-else-if="text === 'done'">Done!</span>
+        <div class="flex justify-between">
+            <div v-if="!started">
+                <span>Click start!</span>
+            </div>
+            <div v-else :class="obscureHint ? 'blur-sm select-none' : ''">
+                <span v-if="text === 'add-to-queue' && visited.length > 0">
+                    Add all of {{ currentVertexName }}'s neighbours (that aren't
+                    already in the queue or visited) to the queue</span
+                >
+                <span
+                    v-else-if="text === 'add-to-queue' && visited.length === 0"
+                >
+                    Add {{ currentVertexName }} to the queue</span
+                >
+                <span v-else-if="text === 'remove-and-set-to-current'"
+                    >Remove {{ queue[0] }} from the queue and set it as the
+                    current vertex</span
+                >
+                <span v-else-if="text === 'visit'"
+                    >Mark {{ currentVertexName }} as visited</span
+                >
+                <span v-else-if="text === 'done'">Done!</span>
+            </div>
+            <div v-if="started" class="ml-2">
+                <span
+                    class="cursor-help"
+                    @mouseenter="
+                        () => {
+                            obscureHint = false;
+                        }
+                    "
+                    @mouseleave="
+                        () => {
+                            obscureHint = true;
+                        }
+                    "
+                    >Hint</span
+                >
+            </div>
         </div>
     </div>
 </template>
