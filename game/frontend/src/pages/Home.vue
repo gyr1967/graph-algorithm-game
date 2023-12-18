caa3fc40a8a0ecaa2ae3ec6c3d035ef163012293
George Yarr <2553638y@student.gla.ac.uk>
2023-12-18 14:40:06 +0000

    show faq component on homepage

--- game/frontend/src/pages/Home.vue
+++ game/frontend/src/pages/Home.vue
@@ -1,6 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FAQ from "../components/FAQ.vue";
+</script>
+
 <template>
-    <div class="flex flex-col items-center h-screen">
-        <h1>the home page</h1>
+    <div class="flex flex-col items-center h-screen mx-2">
+        <h1>Welcome!</h1>
+        <FAQ />
     </div>
 </template>
